2d32ae41fcc9759419a17f6f01cf5c45cf57154a
João Victor Arouca <joao.silveira@estudante.ufla.br>
2021-10-29 00:49:42 -0300

    faltam apenas css

---
+++ Front-End/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
@@ -0,0 +1,11 @@
+import Header from './Header';
+import Pokedex from './Pokedex';
+import './app.css';
+
+function App() {
+  return (
+  <><Header /> <main className='main'><Pokedex /></main></>
+  )
+}
+
+export default App;
